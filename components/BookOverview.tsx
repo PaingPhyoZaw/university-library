8eff3ff757c80729f39beae1c0b5182a19e911f7
PaingPhyoZaw <paingphyozaw54@gmail.com>
2025-01-26 23:52:45 +0630

    create bookoverview component

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Book } from '@/types'
+import Image from 'next/image'
 
 const BookOverview = ({
     title,
@@ -14,9 +15,47 @@ const BookOverview = ({
 }: Book) => {
   return (
     <section className="book-overview">
-        <div className="flex flex-col gap-5">
-            <h1 className="text-4xl font-bold text-center">A long book title</h1>
+      <div className="flex flex-1 flex-col gap-5">
+        <h1>{title}</h1>
+
+        <div className="book-info">
+          <p>
+            By <span className="font-semibold text-light-200">{author}</span>
+          </p>
+
+          <p>
+            Category{" "}
+            <span className="font-semibold text-light-200">{genre}</span>
+          </p>
+
+          <div className="flex flex-row gap-1">
+            <Image src="/icons/star.svg" alt="star" width={22} height={22} />
+            <p>{rating}</p>
+          </div>
         </div>
+
+        <div className="book-copies">
+          <p>
+            Total Books <span>{total_copies}</span>
+          </p>
+
+          <p>
+            Available Books <span>{available_copies}</span>
+          </p>
+        </div>
+
+        <p className="book-description">{description}</p>
+
+      </div>
+
+      <div className="relative flex flex-1 justify-center">
+        <div className="relative">
+
+          <div className="absolute left-16 top-10 rotate-12 opacity-40 max-sm:hidden">
+            
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
